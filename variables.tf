b51f8769d46520066b5b84d4c9ab77a71cf368a5
John Cantu <john.cantu@dreambox.com>
2020-03-22 18:41:03 -0700

    no message

--- variables.tf
+++ variables.tf
@@ -1,27 +1,34 @@
 variable "access_key" {
     type = string
+    default = null
 }
 
 variable "secret_key" {
     type = string
+    default = null
 }
 
 variable "region" {
     type = string
+    default = null
 }
 
 variable "path" {
     type = string
+    default = null
 }
 
 variable "description" {
     type = string
+    default = null
 }
 
 variable "default_lease_ttl_seconds" {
     type = string
+    default = null
 }
 
 variable "max_lease_ttl_seconds" {
     type = string
+    default = null
 }
